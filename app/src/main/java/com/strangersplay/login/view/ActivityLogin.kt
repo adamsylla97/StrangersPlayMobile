b9de667790927695e2b7c7725e2d81a5e2aca4eb
adamsyll <adam.sylla@idemia.com>
2019-10-09 20:53:43 +0200

    added login activity added login layout added dependency for glide added secondary and primary colors

---
+++ app/src/main/java/com/strangersplay/login/view/ActivityLogin.kt
@@ -0,0 +1,15 @@
+package com.strangersplay.login.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.strangersplay.R
+import kotlinx.android.synthetic.main.activity_login.*
+
+class ActivityLogin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
