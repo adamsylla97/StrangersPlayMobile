176cbc56f49b87150cd9c51d8a6a43d7b016edcb
209278 <209278@edu.p.lodz.pl>
2019-10-28 12:02:57 +0100

    Activity to Fragment

--- app/src/main/java/com/strangersplay/single_event/view/SingleEventFragment.kt
+++ app/src/main/java/com/strangersplay/single_event/view/SingleEventFragment.kt
@@ -1,21 +1,29 @@
 package com.strangersplay.single_event.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.bumptech.glide.Glide
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.strangersplay.InstanceProvider
+
 import com.strangersplay.R
 import com.strangersplay.single_event.model.SingleEvent
 import kotlinx.android.synthetic.main.activity_single_event.*
+import kotlinx.android.synthetic.main.activity_single_event.eventContributionTextView
+import kotlinx.android.synthetic.main.activity_single_event.eventImage
+import kotlinx.android.synthetic.main.activity_single_event.eventRatingBar
+import kotlinx.android.synthetic.main.activity_single_event.eventTitleTextView
+import kotlinx.android.synthetic.main.fragment_single_event.*
 
-class SingleEventActivity : AppCompatActivity(), SingleEventView {
+class SingleEventFragment : Fragment(), SingleEventView {
 
     private val singleEventPresenter = InstanceProvider.getSingleEventPresenter(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_single_event)
 
         singleEventPresenter.displaySingleEvent()
     }
@@ -25,5 +33,6 @@ class SingleEventActivity : AppCompatActivity(), SingleEventView {
         Glide.with(this).load(event.eventImage).into(eventImage)
         eventRatingBar.numStars = event.eventLevel
         eventContributionTextView.text = event.eventContribution.toString()
+        localizationTextView.text = event.eventLocalization
     }
 }
