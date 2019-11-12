3cf0c15e867b97f57ecf6c0c0817a8d50cade31c
209278 <209278@edu.p.lodz.pl>
2019-11-12 01:23:02 +0100

    mapview in created event

--- app/src/main/java/com/strangersplay/single_event/view/SingleEventFragment.kt
+++ app/src/main/java/com/strangersplay/single_event/view/SingleEventFragment.kt
@@ -1,11 +1,28 @@
 package com.strangersplay.single_event.view
 
+import android.location.Location
+import android.location.LocationListener
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
+import com.mapbox.android.core.location.LocationEngine
+import com.mapbox.android.core.location.LocationEngineListener
+import com.mapbox.android.core.location.LocationEnginePriority
+import com.mapbox.android.core.location.LocationEngineProvider
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.annotations.MarkerOptions
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode
 import com.strangersplay.InstanceProvider
 
 import com.strangersplay.R
@@ -13,9 +30,18 @@ import com.strangersplay.single_event.model.SingleEvent
 import kotlinx.android.synthetic.main.event_item.view.*
 import kotlinx.android.synthetic.main.fragment_single_event.*
 
-class SingleEventFragment(private val eventId: Int) : Fragment(), SingleEventView {
+class SingleEventFragment(private val eventId: Int) : Fragment(), SingleEventView, PermissionsListener, LocationEngineListener {
+
+    private lateinit var mapView: MapView
+    private lateinit var map: MapboxMap
+    private lateinit var permissionManager: PermissionsManager
+    private lateinit var originLocation: Location
+
+    private var locationEngine: LocationEngine? = null
+    private var locationLayerPlugin: LocationLayerPlugin? = null
 
     private val singleEventPresenter = InstanceProvider.getSingleEventPresenter(this)
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -27,7 +53,14 @@ class SingleEventFragment(private val eventId: Int) : Fragment(), SingleEventVie
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        singleEventPresenter.displaySingleEvent()
+        Mapbox.getInstance(activity?.applicationContext!!, getString(R.string.access_token))
+        mapView = view?.findViewById(R.id.mapView)!!
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync { mapboxMap ->
+            map = mapboxMap
+            enableLocation()
+            singleEventPresenter.displaySingleEvent()
+        }
     }
 
     override fun getEventId(): Int {
@@ -39,6 +72,113 @@ class SingleEventFragment(private val eventId: Int) : Fragment(), SingleEventVie
         Glide.with(this).load("").placeholder(R.drawable.ic_cloud_queue_black_24dp).into(eventImage)
         eventRatingBar.numStars = event.level
         eventContributionTextView.text = event.price.toString()
-        localizationTextView.text = event.eventLocation
+        map.addMarker(MarkerOptions().position(LatLng(51.759445, 19.457216)))
+    }
+
+    private fun enableLocation(){
+        if(PermissionsManager.areLocationPermissionsGranted(activity?.applicationContext)){
+            initializeLocationEngine()
+            initializeLocationLayer()
+        }else{
+            permissionManager = PermissionsManager(this)
+            permissionManager.requestLocationPermissions(activity)
+        }
+    }
+
+    private fun initializeLocationEngine(){
+        locationEngine = LocationEngineProvider(activity?.applicationContext).obtainBestLocationEngineAvailable()
+        locationEngine?.priority = LocationEnginePriority.HIGH_ACCURACY
+        locationEngine?.activate()
+
+        val lastLocation = locationEngine?.lastLocation
+        if(lastLocation != null){
+            originLocation = lastLocation
+        }else{
+            locationEngine?.addLocationEngineListener(this)
+        }
+    }
+
+    private fun initializeLocationLayer(){
+        locationLayerPlugin = LocationLayerPlugin(mapView, map, locationEngine)
+        locationLayerPlugin?.setLocationLayerEnabled(true)
+        locationLayerPlugin?.cameraMode = CameraMode.TRACKING
+        locationLayerPlugin?.renderMode = RenderMode.NORMAL
+    }
+
+    private fun setCameraPosition(location: Location){
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude), 13.0))
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+       if(granted){
+           enableLocation()
+       }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        permissionManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        location?.let {
+            originLocation = location
+            setCameraPosition(location)
+        }
+    }
+
+    override fun onConnected() {
+        locationEngine?.requestLocationUpdates()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(PermissionsManager.areLocationPermissionsGranted(activity?.applicationContext)){
+            locationEngine?.requestLocationUpdates()
+            locationLayerPlugin?.onStart()
+        }
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        locationEngine?.removeLocationUpdates()
+        locationLayerPlugin?.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        locationEngine?.deactivate()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if(outState != null) {
+            mapView.onSaveInstanceState(outState)
+        }
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
